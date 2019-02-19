65953aee0ef9abee81a8b4748f9ab768f90cb4c7
kelvinadeu <kelvinadeu25@gmail.com>
2019-02-19 08:57:27 +0300

    add to models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,18 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class User(UserMixin,db.model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Interger,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    name = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.Column(255))
+    profile_pic_path = db.Column(db.String())
+    the_blog = db.relationship('Blogs',backref = 'user',lazy='dynamic')
+
 class Single:
     def __init__(self,id,author,quotes):
         self.id=id
